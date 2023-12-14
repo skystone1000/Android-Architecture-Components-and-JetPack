e8eb95a58a7c05d27c468d8bf98f8458bb2a0749
Aditya <adityaspmahajan@gmail.com>
2023-12-14 15:07:13 +0530

    01 - JetPack Intro

---
+++ ArchitectureComponents/app/src/main/java/com/example/architecturecomponents/a01intro/A01Theory.kt
@@ -0,0 +1,64 @@
+package com.example.architecturecomponents.a01intro
+
+class A01Theory {
+    /*
+
+------------------------------------------------------------------------------
+01 Android Architecture Components & Jetpack Intro
+
+Brief Overview of Architecture Components
+Problems with existing system
+What is jetpack?
+
+Android Architecture Components
+	- Android has a component-based architecture - where you use different components like Activities, Fragments, Services, etc.
+	- It is Android that manages these components for you
+	- Till now, we have been writing code in our android activities  or fragments for all purposes - whether it is data fetching or storing data in SQLite.
+	- This is where the problem lies.
+
+Architectures
+	- MVC
+	- MVP
+	- MVVM
+
+Logic For different architectures
+	- Separation of concerns
+	- Data Driven Applications
+
+Jetpack Components
+	a) Architecture
+		- Data Binding
+		- Lifecycles
+		- LiveData
+		- Navigation
+		- Paging
+		- Room
+		- ViewModel
+		- WorkManager
+	b) Foundation
+		- AppCompact
+		- Android KTX
+		- Multidex
+		- Test
+	c) UI
+		- Animation & Transition
+		- Auto, TV & Wear
+		- Emoji
+		- Fragment
+		- Layout
+		- Palette
+	d) Behaviour
+		- Download Manager
+		- Media & Playback
+		- Permissions
+		- Notifications
+		- Sharing
+		- Slices
+
+Benefits of Android Architecture Components
+	- Collection of Libraries that helps build robust, testable and maintainable android apps.
+	- Less Boilerplate code - Room, Navigation Components, Paging, Etc
+	- High Quality, Production ready Apps
+
+     */
+}
